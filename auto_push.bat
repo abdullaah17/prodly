c7e893d78bdfd7ef9dfe02106a865e46d27b536d
abdullah17 <bachelorabdullah17@gmail.com>
2025-12-18 18:24:05 +0500

    auto update

--- auto_push.bat
+++ auto_push.bat
@@ -1,13 +1,8 @@
 @echo off
 cd /d %~dp0
 
-REM Stage all tracked and untracked files, ignore build/dist/ thanks to .gitignore
 git add -A
-
-REM Commit changes (allow empty)
 git commit -m "auto update" --allow-empty
-
-REM Push to GitHub
 git push
 
 pause
